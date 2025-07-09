1b0a7ddd919735c0d8accbc7556d309b8692b546
Smartdevs17 <smartdevs17@gmail.com>
2025-07-09 14:04:56 +0100

    fix: remove config.json

--- backend/config/sequilize.ts
+++ backend/config/sequilize.ts
@@ -11,7 +11,7 @@ const sequelize = new Sequelize(
     host: process.env.DB_HOST || "localhost",
     port: Number(process.env.DB_PORT) || 5432,
     dialect: "postgres",
-    logging: true,
+    logging: false,
     dialectOptions: {
       ssl: {
         require: true,
